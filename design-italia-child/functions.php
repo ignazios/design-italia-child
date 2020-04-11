77fa53551ca2822077ecec3706c06903fbae494f
Ignazios <ignazios@gmail.com>
2020-04-11 10:08:00 +0200

    pubblicazione versione 0

--- design-italia-child/functions.php
+++ design-italia-child/functions.php
@@ -6,6 +6,12 @@
  *
  * @package design-italia-child
  */
+  /* UPDATER THEME VERSION */
+require 'inc/theme-update-checker.php';
+$update_checker = new ThemeUpdateChecker(
+    'wpscuola',
+    'https://raw.githubusercontent.com/ignazios/wpscuola/master/wpscuola.json');
+
  if ( function_exists( 'add_theme_support' ) ) { 
     add_image_size( 'img-wrapper-thumb', 660, 300, true);
  }
